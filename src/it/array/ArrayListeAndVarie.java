e3d754c17b3bee836e7393c93e6e6b23deb84901
Marco <marco.chianese93@gmail.com>
2021-04-27 12:02:58 +0200

    Creazione Classe prodotti ed estensione nel main

--- src/it/array/ArrayListeAndVarie.java
+++ src/it/array/ArrayListeAndVarie.java
@@ -1,5 +1,19 @@
 package it.array;
 
 public class ArrayListeAndVarie {
-
+	public static void main(String[] args) {
+		
+		Prodotti p = new Prodotti() ;
+		
+		
+		p.setNome("penna");
+		
+		p.setMarca("bic");
+		p.setPrezzo(0.50);
+		
+		
+		
+		
+		System.out.println(p.toString());
+	}
 }
---
+++ src/it/array/Prodotti.java
@@ -0,0 +1,53 @@
+package it.array;
+
+public class Prodotti {
+	private String nome ;
+	private String marca ;
+	private double prezzo ;
+	
+	public Prodotti() {}
+	
+	public Prodotti( String nome, String marca , double prezzo) {
+		
+		this.nome = nome;
+		this.marca = marca;
+		this.prezzo = prezzo;
+				
+	}
+	
+//	Creo i metodi get e set per la variabile Nome 
+	
+	 public String  getNome() {
+		 return nome ;
+	 }
+	 public void setNome(String nome) {
+		 this.nome = nome;
+	 }
+	
+//	 Creo i metodi get() e set() per la variabile marca
+	 
+	 public String getMarca() {
+		 return marca;
+	 }
+	 
+	 public void setMarca(String marca) {
+		 this.marca = marca ;
+	 }
+	 
+//	 Creo i metodi get() e set() per la variabile prezzo
+  	 
+	 public double getPrezzo() {
+		 return prezzo;
+	 }
+	 public void setPrezzo(double prezzo) {
+		 this.prezzo = prezzo;
+	 }
+	 
+//	 Sovrascrivo tramite @Override 
+	 
+	 @Override
+	 public String toString() {
+		 return "Prodotti " + "nome: " +  nome + " marca: " + marca + " prezzo: " + prezzo  ;
+	 }
+	 
+}
